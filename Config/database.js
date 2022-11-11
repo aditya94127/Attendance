9d064db9d25832d47228127f9bc84b679b6cb82c
aditya94127 <adityamishra94127@gmail.com>
2022-11-11 21:30:44 +0530

    Fix

--- Config/database.js
+++ Config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 const connection =()=>{
-    mongoose.connect(process.env.MONGO_URL).then((res)=>console.log("connection successfull")).catch((err)=>console.log(err))
+    mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology}).then((res)=>console.log("connection successfull")).catch((err)=>console.log(err))
 }
 
 module.exports=connection
